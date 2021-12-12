d979a4dce13eb87fdc5e81193d569fe9a7da6e93
Julia Veremienko <yuli4ka.ver27@gmail.com>
2021-12-13 01:01:57 +0200

    удаление коментариев, изменение в 9 строке

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <QApplication>
 #include <QPushButton>
- //there should be changes from the first branch
- // there are mustbe some change too
+ 
+
 int main(int argc, char *argv[ ]) 
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
+QPushButton button("Hi, World!");
 button.resize(200, 60);
 button.show( );
  
